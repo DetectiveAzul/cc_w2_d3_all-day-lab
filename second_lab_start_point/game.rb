3717e91b6803c334e7ccf11edb6c4c25653eeb42
Jaime Lopez <detec.azul@gmail.com>
2018-03-14 14:33:06 +0000

    Second lab structure folder created

---
+++ second_lab_start_point/game.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/rg'
+require_relative '../game'
+
+class GameTest < Minitest::Test
+  def setup
+  end
+
+  def test_
+  end
+
+end
---
+++ second_lab_start_point/player.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/rg'
+require_relative '../player'
+
+class PlayerTest < Minitest::Test
+  def setup
+  end
+
+  def test_
+  end
+
+end
